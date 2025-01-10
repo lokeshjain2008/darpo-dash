b400231f90b98e01e051ef228d3a41f2a490ebca
Lokesh Kumar Jain <lokeshjain2008@gmail.com>
2025-01-11 02:23:58 +0530

    feat: Add utility components and loading states

---
+++ src/lib/config.ts
@@ -0,0 +1,29 @@
+export const siteConfig = {
+  name: 'Darpo Dashboard',
+  description: 'Property Management Dashboard',
+  links: {
+    github: 'https://github.com/lokeshjain2008/darpo-dash',
+    docs: 'https://docs.darpo.in',
+  },
+};
+
+export const navigationConfig = {
+  mainNav: [
+    {
+      title: 'Dashboard',
+      href: '/',
+    },
+    {
+      title: 'Organizations',
+      href: '/organizations',
+    },
+    {
+      title: 'Properties',
+      href: '/properties',
+    },
+    {
+      title: 'Users',
+      href: '/users',
+    },
+  ],
+};
